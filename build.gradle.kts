2b2f0ec5012ad1d26441db7a7988f621543a8952
Carolina Torrente <121116840+CarolinaTorrente@users.noreply.github.com>
2025-03-09 11:52:24 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+}
